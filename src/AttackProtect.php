e4a9e2594e8b9c89808bcdfd292c7e08ccda4c90
Filip Sedivy <mail@filipsedivy.cz>
2015-05-30 02:29:33 +0200

    Úprava licence

--- src/AttackProtect.php
+++ src/AttackProtect.php
@@ -1,7 +1,7 @@
 <?php
 
 /* 
- * Copyright (C) 2015 Filip
+ * Copyright (C) 2013 - 2015, Filip Šedivý
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
